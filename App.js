f9abe0f61f92acaefd981c1dfbb061a9b99c13c6
Ant Tech <anttech@Ants-Mac-mini.Home>
2018-12-21 14:07:14 +0700

    view list

--- App.js
+++ App.js
@@ -16,10 +16,39 @@ class HomeScreen extends React.Component {
 
 	}
 
-	_renderItem = item => {
+	_renderItem = ({item}) => {
 		return (
-			<View>
-				
+			<View  style={styles.taskInfo}>
+				<View style={styles.item}>
+					<View>
+						<Text>
+							{item.info}
+						</Text>
+					</View>
+					<View style={styles.timeView}>
+						<View>
+							<Text>
+								{item.time}
+							</Text>
+						</View>
+						<View  style={{marginLeft: 10}}>
+							{item.completed
+								? <Icon.MaterialCommunityIcons name="checkbox-marked-circle" size={30} color='green' />
+								: <Icon.MaterialCommunityIcons name="checkbox-blank-circle-outline" size={30} color='green'/>
+							}
+						</View>
+					</View>
+				</View>
+			</View>
+		)
+	}
+
+	_renderHeader = ({section}) => {
+		return (
+			<View style={styles.taskTitle}>
+				<Text>
+					{String(section.title).toUpperCase()}
+				</Text>
 			</View>
 		)
 	}
@@ -28,43 +57,47 @@ class HomeScreen extends React.Component {
 		const text = 'ngay thang nam'
 		return (
 			<View style={styles.container}>
-				<SectionList 
-					sections={[
-						{
-							title: 'feb 10 2016', 
-							data: [
-								{info: 'interview at google', time: '12: 30PM', completed: false},
-								{info: 'Pick up the kids', time: '412: 30PM', completed: false}
-							]
-						},
-						{
-							title: 'feb 9 2016',
-							data: [
-								{info: 'Brain`s birthday party', time: '7:00PM', completed: false},
-								{info: 'First project due and start second project', time: '9:30AM', completed: false}
-							]
-						},
-						{
-							title: 'feb 6 2016',
-							data: [
-								{info: 'Finish the first iteration before sleep', time: '9:00PM', completed: true},
-								{info: 'Dinner at Partycity', timne: '7:00PM', completed: true},
-								{info: 'Bring latop to the meeting', time: '8:00AM', completed: true},
-								{info: 'Leg day', time: '6:00AM', completed: true}
-							]
-						},
-						{
-							title: 'feb 6 2016',
-							data: [
-								{info: 'Finish the first iteration before sleep', time: '9:00PM', completed: true},
-								{info: 'Dinner at Partycity', timne: '7:00PM', completed: true},
-								{info: 'Bring latop to the meeting', time: '8:00AM', completed: true},
-								{info: 'Leg day', time: '6:00AM', completed: true}
-							]
-						},
-					]}
-					renderItem={}
-				/>
+				<View style={styles.list}>
+					<SectionList 
+						sections={[
+							{
+								title: 'feb 10 2016', 
+								data: [
+									{info: 'interview at google', time: '12:30PM', completed: false},
+									{info: 'Pick up the kids', time: '4:30PM', completed: false}
+								]
+							},
+							{
+								title: 'feb 9 2016',
+								data: [
+									{info: 'Brain`s birthday party', time: '7:00PM', completed: false},
+									{info: 'First project due and start second project', time: '9:30AM', completed: false}
+								]
+							},
+							{
+								title: 'feb 6 2016',
+								data: [
+									{info: 'Finish the first iteration before sleep', time: '9:00PM', completed: true},
+									{info: 'Dinner at Partycity', time: '7:00PM', completed: true},
+									{info: 'Bring latop to the meeting', time: '8:00AM', completed: true},
+									{info: 'Leg day', time: '6:00AM', completed: true}
+								]
+							},
+							{
+								title: 'feb 6 2016',
+								data: [
+									{info: 'Finish the first iteration before sleep', time: '9:00PM', completed: true},
+									{info: 'Dinner at Partycity', time: '7:00PM', completed: true},
+									{info: 'Bring latop to the meeting', time: '8:00AM', completed: true},
+									{info: 'Leg day', time: '6:00AM', completed: true}
+								]
+							},
+						]}
+						renderItem={this._renderItem}
+						renderSectionHeader={this._renderHeader}
+						keyExtractor={(item, index) => index}
+					/>
+				</View>
 			</View>
 		);
 	}
@@ -88,8 +121,8 @@ const styles = StyleSheet.create({
 	container: {flex: 1, backgroundColor: "#D3D3D3",},
 	list: {  margin: 5 },
 	task: { marginBottom: 10, marginTop: 10,},
-	taskTitle: { margin: 5},
-	taskInfo: {backgroundColor: 'white', padding: 10,},
+	taskTitle: { margin: 10},
+	taskInfo: {backgroundColor: 'white', padding: 15,},
 	item: {flexDirection: 'row',justifyContent: 'space-between', alignItems: 'center', margin: 5},
 	timeView:{flexDirection: 'row',justifyContent: 'space-between', alignItems: 'center'}
 });
